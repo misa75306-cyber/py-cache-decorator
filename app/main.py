33fd527840ddb7167fde7a75673c93960c74bda3
misa75306-cyber <misa75306@gmail.com>
2026-06-13 22:33:48 +0300

    feat 3: add cache decorator and tests

--- app/main.py
+++ app/main.py
@@ -6,11 +6,14 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs) -> Callable:
         key = (args, tuple(sorted(kwargs.items())))
+
         if key in storage:
             print("Getting from cache")
             return storage[key]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
+            storage[key] = result  # <-- ОСЬ ЦЕЙ РЯДОК ОБОВ'ЯЗКОВО ТРЕБА ДОДАТИ!
             return result
+
     return wrapper
