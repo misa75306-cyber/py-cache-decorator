40c300413c9e67f88813718033db97a772d82517
misa75306-cyber <misa75306@gmail.com>
2026-06-13 22:20:29 +0300

    feat: add cache decorator and tests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
+from collections.abc import dict_items
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in storage:
+            print("Getting from cache")
+            return storage[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            return result
+    return wrapper
